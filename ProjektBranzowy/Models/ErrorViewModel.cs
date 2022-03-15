5320e917171c76afbf29fd0b7ea18ddbd9a5925c
Dawid Borowik <dawid.borowik@student.wsg.pl>
2022-03-15 21:25:22 +0100

    Add project files.

---
+++ ProjektBranzowy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjektBranzowy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

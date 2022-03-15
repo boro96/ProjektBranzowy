94bc9969e90924bedeae8413d520d8e68a95ca0a
Dawid Borowik <dawid.borowik@student.wsg.pl>
2022-03-15 22:04:06 +0100

    Added ClassModels

---
+++ ProjektBranzowy/Models/LogHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektBranzowy.Models
+{
+    public class LogHistory
+    {
+        public int LogHistoryId { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime In { get; set; }
+        public DateTime Out { get; set; }
+    }
+}
---
+++ ProjektBranzowy/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektBranzowy.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public string  Name { get; set; }
+    }
+}

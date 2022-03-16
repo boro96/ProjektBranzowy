d7e85f43f8aece9a85ce1744c1af3f8904dfbee1
Dawid Borowik <dawid.borowik@student.wsg.pl>
2022-03-16 12:20:55 +0100

    Add ScheduleModel

---
+++ ProjektBranzowy/Models/Schedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektBranzowy.Models
+{
+    public class Schedule
+    {
+        public int ScheduleId { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public TimeSpan Start { get; set; }
+        public TimeSpan Finish { get; set; }
+
+    }
+}
